c0b8ebc11e4e2b5a9f4f702d15629a90575183a8
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-06 21:51:11 -0500

    - Move all properties to application.properties

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -14,18 +14,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
  * @Date : 05/06/20, Wed
  */
 @Configuration
-//Any of the following are valid and its up to your preference which one use.
-//@PropertySource({"classpath:datasource.properties",
-                // "classpath:jms".properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    //To read environment variables
-    //@Autowired
-    //Environment environment;
 
     @Value("${database.user}")
     String user;
@@ -49,7 +38,6 @@ public class PropertyConfig {
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUser(user);
-        //fakeDataSource.setUser(environment.getProperty("ENVIRONMENT_VAR"));
         fakeDataSource.setPwd(pwd);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
